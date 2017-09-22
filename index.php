9eb9d4bd7951c072d7d29aa9e9cd63c2c9e047be
Marco Alejandro Martinez <martinezsuarezmarcoalejandro@gmail.com>
2017-09-21 23:30:39 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="es">
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="css/normalize.css">
+<link rel="stylesheet" type="text/css" href="css/materialize.css">
+<link rel="stylesheet" type="text/css" href="fonts/icons/material-icons.css">
+<script type="text/javascript" src="js/jquery.js"></script>
+<script src="js/materialize.js"></script>
+<head>
+	<title>Contactos</title>
+</head>
+<body>
+	<nav class="sidenav blue lighten-3">
+		<ul>
+			<li><a href="">Hola</a></li>
+			<li><a href="">Adios</a></li>
+		</ul>
+	</nav>
+	<section class="container">
+		<div class="row ">
+			<div class="col">
+				<div class="card-panel row offset-s2 s12 m6 l4 left-align">
+					<div class="col m4 s8 l4">
+						<img src="img/owl.jpg" class="circle" width="100px" >
+					</div>
+					<div class="col m6 s10 l8">
+						<p>Marco Alejandro Martinez Suarez <br>
+					Teléfono: 7261048407</p>
+					</div>
+				</div>
+			</div>
+			<div class="col s12 m5 l6">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</div>
+		</div>
+	</section>
+	<footer class="page-footer blue">
+		<div class="row">
+			<div class="col m4 l4 offset-l2 s8 offset-12 offset-m4 offset-s2 center-align">
+				<img src="img/owl.jpg" class="circle" width="40%" >
+			</div>
+			<div class="col m4 l4 m8 offset-s2 offset-m2 left-align">
+				<ul>
+					<li>
+						<h6 style="font-weight: 300"><i class="material-icons">contacts</i> Marco Alejandro Martinez Suarez</h6>
+					</li>
+					<li>
+						<h6 style="font-weight: 300"><i class="material-icons">search</i> /marco.a.martinez.52035</h6>
+					</li>
+					<li>
+						<h6 style="font-weight: 300"><i class="material-icons">person</i> Web Developer</h6>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
